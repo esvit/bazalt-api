61edc93316d00330b803a907f3024670daa02c6b
Alex Slubsky <aslubsky@gmail.com>
2013-10-25 16:28:06 +0300

    add options save

---
+++ src/Components/Sites/Webservice/OptionsResource.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Components\Sites\Webservice;
+use Bazalt\Rest\Response,
+    Bazalt\Site\Model\Option;
+
+/**
+ * OptionsResource
+ *
+ * @priority 100
+ * @uri /sites/options
+ */
+class OptionsResource extends \Bazalt\Rest\Resource
+{
+    /**
+     * @method POST
+     * @json
+     */
+    public function saveItem()
+    {
+      /*  if (!\Bazalt\Auth::getUser()->hasPermission('admin.access')) {
+            return new Response(Response::FORBIDDEN, ['user' => 'Permission denied']);
+        }*/
+
+//        var_dump($this->request->data);exit;
+        foreach($this->request->data as $name => $value) {
+            \Bazalt\Site\Option::set($name, $value);
+        }
+//var_dump($data);exit;
+        return new Response(Response::OK, $this->request->data);
+    }
+}
