df5320b6e9438169561fc45e07c6af8ad2794c04
Vitalii Savchuk <esvit666@gmail.com>
2013-11-06 15:19:26 +0200

    add support of php 5.3

--- src/Components/Users/Model/GiftRefUser.php
+++ src/Components/Users/Model/GiftRefUser.php
@@ -4,6 +4,14 @@ namespace Components\Users\Model;
 
 class GiftRefUser extends Base\GiftRefUser
 {
+    public static function getById($id)
+    {
+        $q = GiftRefUser::select()
+                ->where('item_id = ?', (int)$id);
+
+        return new \Bazalt\ORM\Collection($q);
+    }
+
     public static function getCollection()
     {
         $q = GiftRefUser::select();
