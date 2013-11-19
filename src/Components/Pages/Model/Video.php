bd8eaee03941cf2a2c62e20be2c36cfa8eb2b61d
Vitalii Savchuk <esvit666@gmail.com>
2013-11-19 14:23:31 +0200

    change uploader

--- src/Components/Pages/Model/Video.php
+++ src/Components/Pages/Model/Video.php
@@ -15,10 +15,10 @@ class Video extends Base\Video
         $image_url = parse_url($url);
         if ($image_url['host'] == 'www.youtube.com' || $image_url['host'] == 'youtube.com') {
             $array = explode("&", $image_url['query']);
-            return "http://img.youtube.com/vi/" . substr($array[0], 2) . "/0.jpg";
+            return "http://img.youtube.com/vi/" . substr($array[0], 2) . "/hqdefault.jpg";
         } else if ($image_url['host'] == 'www.youtu.be' || $image_url['host'] == 'youtu.be') {
             $array = explode("/", $image_url['path']);
-            return "http://img.youtube.com/vi/" . $array[1] . "/default.jpg";
+            return "http://img.youtube.com/vi/" . $array[1] . "/hqdefault.jpg";
         } else if ($image_url['host'] == 'www.vimeo.com' || $image_url['host'] == 'vimeo.com') {
             $hash = unserialize(file_get_contents("http://vimeo.com/api/v2/video/" . substr($image_url['path'], 1) . ".php"));
             return $hash[0]["thumbnail_small"];
