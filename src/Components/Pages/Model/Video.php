8a8fb9f998003cc14bcc9a04e196746cfee6c6f2
Vitalii Savchuk <esvit666@gmail.com>
2013-11-19 14:20:27 +0200

    change uploader

--- src/Components/Pages/Model/Video.php
+++ src/Components/Pages/Model/Video.php
@@ -9,4 +9,28 @@ class Video extends Base\Video
         $video = new Video();
         return $video;
     }
+
+    protected function video_image($url)
+    {
+        $image_url = parse_url($url);
+        if ($image_url['host'] == 'www.youtube.com' || $image_url['host'] == 'youtube.com') {
+            $array = explode("&", $image_url['query']);
+            return "http://img.youtube.com/vi/" . substr($array[0], 2) . "/0.jpg";
+        } else if ($image_url['host'] == 'www.youtu.be' || $image_url['host'] == 'youtu.be') {
+            $array = explode("/", $image_url['path']);
+            return "http://img.youtube.com/vi/" . $array[1] . "/default.jpg";
+        } else if ($image_url['host'] == 'www.vimeo.com' || $image_url['host'] == 'vimeo.com') {
+            $hash = unserialize(file_get_contents("http://vimeo.com/api/v2/video/" . substr($image_url['path'], 1) . ".php"));
+            return $hash[0]["thumbnail_small"];
+        }
+    }
+
+    public function toArray()
+    {
+        $res = parent::toArray();
+
+        $res['thumbnails'] = $this->video_image($this->url);
+
+        return $res;
+    }
 }
