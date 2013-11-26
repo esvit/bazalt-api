70ec7cef8ff66511c347c332eac6d71ed3c5a946
Vitalii Savchuk <esvit666@gmail.com>
2013-11-26 13:43:27 +0200

    add test for truncate

--- src/Components/Pages/Model/Video.php
+++ src/Components/Pages/Model/Video.php
@@ -46,7 +46,8 @@ class Video extends Base\Video
 
         $res['image_url'] = $this->getImage();
         $res['thumbnails'] = [
-            'main' => thumb($res['image_url'], '220x220', ['fit' => true, 'crop' => true])
+            'main' => thumb($res['image_url'], '220x220', ['fit' => true, 'crop' => true]),
+            'smallthumb' => thumb($this->url, '107x107', ['fit' => true, 'crop' => true])
         ];
 
         return $res;
