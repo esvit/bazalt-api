581081e937cd80fefa1fa73c1aba1b29ebd7b8ea
Alex Slubsky <aslubsky@gmail.com>
2013-12-19 09:31:44 +0200

    Update Uploader.php

--- src/CMS/Uploader.php
+++ src/CMS/Uploader.php
@@ -7,12 +7,12 @@ class Uploader extends \Bazalt\Rest\Uploader
     public function uploadTo($type)
     {
         $siteId = \Bazalt\Site::getId();
-        $file = $this->handleUpload(SITE_DIR . '/uploads', [$siteId, $type]);
+        $result = $this->handleUpload(SITE_DIR . '/uploads', [$siteId, $type]);
 
         /*$result = [];
         $json = file_get_contents(SITE_DIR . '/thumbnails.json');
         $thumbnails = json_decode($json, true);
         print_R($thumbnails);exit;*/
-        return $file;
+        return $result['file'];
     }
 }
