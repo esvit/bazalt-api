928df726402410f731a40df7fb77e7832aa70c11
Vitalii Savchuk <esvit666@gmail.com>
2014-02-08 15:11:07 +0200

    `

--- src/Components/Pages/Model/Image.php
+++ src/Components/Pages/Model/Image.php
@@ -23,11 +23,14 @@ class Image extends Base\Image
             'thumbnailUrl' => thumb($this->url, '100x100', ['crop' => true, 'fit' => true]),
             'size' => (double)$this->size,
             'thumbnails' => [
-                'preview' => thumb($this->url, '160x100', ['fit' => true]),
+                /*'preview' => thumb($this->url, '160x100', ['fit' => true]),
                 'main' => thumb($this->url, '220x220', ['fit' => true, 'crop' => true]),
                 'person' => thumb($this->url, '460x450', ['fit' => true, 'crop' => true]),
                 'smallthumb' => thumb($this->url, '107x107', ['fit' => true, 'crop' => true]),
-                'face' => thumb($this->url, '100x100', ['fit' => true, 'crop' => true])
+                'face' => thumb($this->url, '100x100', ['fit' => true, 'crop' => true])*/
+                'large' => thumb($this->url, '460x290', ['fit' => true, 'crop' => true]),
+                'medium' => thumb($this->url, '350x230', ['fit' => true, 'crop' => true]),
+                'small' => thumb($this->url, '80x60', ['fit' => true, 'crop' => true])
             ]
         ];
 
